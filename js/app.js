aee336ddaabdd85614463b70aa4bc83948d2f001
dev_nayan <md.nayan8494@gmail.com>
2022-07-07 16:52:02 +0600

    Navlink modified and ToTop Button added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
+// Navbar 
+
+let navbar = document.querySelector('.navbar');
+
+const scroll = window.scrollY;
+
+document.addEventListener('scroll', () => {
+    if(scrollY > 50) {
+        navbar.style.background = '#161616';
+        navbar.style.borderBottom = '2px solid #ffffff43';
+        navbar.style.transition = '0.3s ease';
+    } else {
+        navbar.style.background = 'transparent';
+        navbar.style.border = 'none';
+        navbar.style.transition = '0.3s ease';
+    }
+})
+
 // Hamburger Menu 
 
 let hamBtn = document.querySelector('.navbar-toggler');
@@ -21,6 +39,23 @@ let searchBox = document.querySelector('.search-box');
 
 searchIcon.addEventListener('click', () => {
     searchBox.classList.toggle('opacity');
+})
+
+
+// ToTop Button
+
+let toTop = document.getElementById('toTop');
+
+document.addEventListener('scroll', () => {
+    if (scrollY > 650) {
+        toTop.style.bottom = '20px';
+    } else {
+        toTop.style.bottom = '-50px';
+    }
+})
+
+toTop.addEventListener('click', () => {
+    window.scrollTo(0, 0);
 })
 
 
